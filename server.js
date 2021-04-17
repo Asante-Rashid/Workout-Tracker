fa58563f2b9a850ea337304b3f5f7fdcb35d3d76
Asante <asanterashid.biz@gmail.com>
2021-04-17 15:58:43 -0400

    added page links to server

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "./public/index.html"));
 });
 
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/stats.html"));
+});
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
   });
