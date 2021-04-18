5bff26083f8ec75cdf0789df1cc7486d876001b6
Asante <asanterashid.biz@gmail.com>
2021-04-18 13:42:28 -0400

    stats dashboard renders

--- routes/api.js
+++ routes/api.js
@@ -28,13 +28,24 @@ router.put("/api/workouts/:id", ({ body }, res) => {
 
 router.get("/api/workouts", (req, res) => {
     Workout.find({})
-      .sort({ date: -1 })
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
+        .sort({ date: -1 })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+        .sort({ date: -1 })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 
 module.exports = router;
